49abebcbacbb6619764260c38220f23140abae5c
mengwenkai1 <mengwenkai1@163.com>
2019-07-12 09:31:06 +0800

    孟文凯1

---
+++ open_show/src/main/java/fragment/Fragmentthree.java
@@ -0,0 +1,17 @@
+package fragment;
+
+import com.example.open_show.R;
+
+import zhang.bw.com.common.core.WDFragment;
+
+public class Fragmentthree extends WDFragment {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragmentthree;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}

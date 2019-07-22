d154a4d711d3165563354147896baeac95b095c3
mengwenkai1 <mengwenkai1@163.com>
2019-07-22 11:43:58 +0800

    孟文凯

--- common/src/main/java/zhang/bw/com/common/core/http/IAppRequest.java
+++ common/src/main/java/zhang/bw/com/common/core/http/IAppRequest.java
@@ -10,15 +10,13 @@ import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.PUT;
-import retrofit2.http.Path;
-import retrofit2.http.Header;
 import retrofit2.http.Query;
 import zhang.bw.com.common.bean.BannerBean;
-import zhang.bw.com.common.bean.Byliebiao;
-import zhang.bw.com.common.bean.HbchaXun;
 import zhang.bw.com.common.bean.BingBean;
+import zhang.bw.com.common.bean.Byliebiao;
 import zhang.bw.com.common.bean.CXBean;
 import zhang.bw.com.common.bean.GameBean;
+import zhang.bw.com.common.bean.HbchaXun;
 import zhang.bw.com.common.bean.JanBean;
 import zhang.bw.com.common.bean.LoginBean;
 import zhang.bw.com.common.bean.MyjiankangBean;
@@ -30,9 +28,8 @@ import zhang.bw.com.common.bean.ShowBean;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
-import zhang.bw.com.common.bean.ShowBeans;
+import zhang.bw.com.common.bean.Result;
 import zhang.bw.com.common.bean.YaoBean;
-import zhang.bw.com.common.bean.YishengBean;
 
 /**
  * @author dingtao
@@ -42,7 +39,6 @@ import zhang.bw.com.common.bean.YishengBean;
 public interface IAppRequest {
     @GET("share/v1/bannersShow")
     Observable<Result<List<BannerBean>>>bannersShow();
-
     @GET("share/knowledgeBase/v1/findDepartment")
     Observable<Result<List<ShowBean>>>findDepartment();
     @GET("share/knowledgeBase/v1/findDrugsCategoryList")
@@ -51,8 +47,24 @@ public interface IAppRequest {
     Observable<Result<List<YaoBean>>>findDrugsKnowledgeList(@Query("drugsCategoryId")String drugsCategoryId, @Query("page")String page, @Query("count")String count);
     @GET("share/information/v1/findInformationList")
     Observable<Result<List<JanBean>>>findInformationList(@Query("plateId")String plateId, @Query("page")String page, @Query("count")String count);
+    @GET("share/information/v1/findInformationList")
+    Observable<Result<List<JanBean>>>findDrugsKnowledgeList1(@Query("plateId")String plateId, @Query("page")String page, @Query("count")String count);
     @GET("share/knowledgeBase/v1/findDiseaseCategory")
     Observable<Result<List<BingBean>>>findDiseaseCategory(@Query("departmentId")String departmentId);
+    @GET("user/video/v1/findVideoCategoryList")
+    Observable<Result<List<NameBean>>>findVideoCategoryList();
+    @GET("user/video/v1/findVideoVoList")
+    Observable<Result<List<GameBean>>>findVideoVoList(@Header("userId")long userId, @Header("sessionId")String sessionId, @Query("categoryId")String categoryId, @Query("page")String page, @Query("count")String count);
+    @GET("user/video/v1/findVideoCommentList")
+    Observable<Result<List<PingBean>>>findVideoCommentList(@Query("videoId")String videoId);
+    @FormUrlEncoded
+    @POST("user/video/verify/v1/addUserVideoCollection")
+    Observable<Result>addUserVideoCollection(@Header("userId")long userId, @Header("sessionId")String sessionId,@Field("videoId")String videoId);
+    @GET("user/verify/v1/findUserWallet")
+    Observable<Result>findUserWallet1(@Header("userId")long userId, @Header("sessionId")String sessionId);
+    @FormUrlEncoded
+    @POST("user/video/verify/v1/videoBuy")
+    Observable<Result>videoBuy(@Header("userId")long userId, @Header("sessionId")String sessionId,@Field("videoId")String videoId,@Field("price")int price);
     //发送邮箱验证码
     @FormUrlEncoded
     @POST("user/v1/sendOutEmailCode")
@@ -69,51 +81,60 @@ public interface IAppRequest {
     @POST("user/v1/login")
     Observable<Result<LoginBean>> login(@Field("email")String email,
                                         @Field("pwd")String pwd);
-    //头像上传
-    @POST("user/verify/v1/modifyHeadPic")
-    Observable<Result<String>> modifyHeadPic(@Header("userId")long userId,
-                                               @Header("sessionId")String sessionId,
-                                               @Body MultipartBody body);
-    //根据用户ID查询用户信息
-    @GET("user/verify/v1/getUserInfoById")
-    Observable<Result<CXBean>> getUserInfoById(@Header("userId") long userId,
-                                               @Header("sessionId") String sessionId);
-    //检验验证码
-    @FormUrlEncoded
-    @POST("user/v1/checkCode")
-    Observable<Result> checkCode(@Field("email")String email,
-                                 @Field("code")String code);
-    //重置用户密码（忘记密码用）
-    @PUT("user/v1/resetUserPwd")
-    Observable<Result> resetUserPwd(@Query("email") String email,
-                                    @Query("pwd1") String pwd1,
-                                    @Query("pwd2")String pwd2);
+//    //头像上传
+//    @POST("usererify1/modifyHeadPic")
+//    Observable<Result<String>> modifyHeadPic(@Header("userId")long userId,
+//                                             @Header("sessionId")String sessionId,
+//                                             @Body MultipartBody body);
+//    //根据用户ID查询用户信息
+//    @GET("usererify1/getUserInfoById")
+//    Observable<Result<CXBean>> getUserInfoById(@Header("userId") long userId,
+//                                               @Header("sessionId") String sessionId);
+//    //检验验证码
+//    @FormUrlEncoded
+//    @POST("user1eckCode")
+//    Observable<Result> checkCode(@Field("email")String email,
+//                                 @Field("code")String code);
+//    //重置用户密码（忘记密码用）
+//    @PUT("user1/resetUserPwd")
+//    Observable<Result> resetUserPwd(@Query("email") String email,
+//                                    @Query("pwd1") String pwd1,
+//                                    @Query("pwd2")String pwd2);
+//
+//    //支付
+//    @FormUrlEncoded
+//    @POST("usererify1/recharge")
+//    Observable<Result> zhifujiekou(@Header("userId") long id, @Header("sessionId") String sessionId,
+//                                   @Field("money") String money, @Field("payType") String payType);
 
-    //支付
-    @FormUrlEncoded
-    @POST("user/verify/v1/recharge")
-    Observable<Result> zhifujiekou(@Header("userId") long id, @Header("sessionId") String sessionId,
-                                   @Field("money") String money, @Field("payType") String payType);
-
-    //我的钱包
-    @GET("user/verify/v1/findUserWallet")
-    Observable<Result<Integer>> Memoney(@Header("userId") long id, @Header("sessionId") String sessionId);
-
-
-
-    //医生信息
-    @GET("user/inquiry/v1/findDoctorList")
-    Observable<Result> Yisheng (@Header("userId") long id, @Header("sessionId") String sessionId,
-    @Query("deptId") String deptId, @Query("condition") String condition,@Query("sortBy") String sortBy,
-                                                   @Query("page") String page,@Query("count") String count);
-
-    //病友圈列表
-    @GET("user/sickCircle/v1/findSickCircleList")
-    Observable<Result<List<Byliebiao>>> SickCircle(@Query("departmentId") String departmentId,
-                                                   @Query("page") String page,@Query("count") String coun);
-
-        //关键字搜索病友圈
-    @GET("user/sickCircle/v1/searchSickCircle")
-    Observable<Result<List<Byliebiao>>> guanjianzi(@Query("keyWord") String keyWord);
+//    //我的钱包
+//    @GET("usererify1/findUserWallet")
+//    Observable<Result<Integer>> Memoney(@Header("userId") long id, @Header("sessionId") String sessionId);
+//
+//
+//
+//    //医生信息
+//    @GET("user/inquiry1/findDoctorList")
+//    Observable<Result> Yisheng (@Header("userId") long id, @Header("sessionId") String sessionId,
+//                                @Query("deptId") String deptId, @Query("condition") String condition,@Query("sortBy") String sortBy,
+//                                @Query("page") String page,@Query("count") String count);
+//
+//    //病友圈列表
+//    @GET("user/sickCircle1/findSickCircleList")
+//    Observable<Result<List<Byliebiao>>> SickCircle(@Query("departmentId") String departmentId,
+//                                                   @Query("page") String page,@Query("count") String coun);
+//
+//    //关键字搜索病友圈
+//    @GET("user/sickCircle1archSickCircle")
+//    Observable<Result<List<Byliebiao>>>guanjianzi(@Query("keyWord") String keyWord);
+//    //请求H币信息
+//    @GET("usererify1/findHealthyCurrencyNoticeList")
+//    Observable<Result<List<HbchaXun>>> Hchaxun(@Header("userId") long id, @Header("sessionId") String sessionId,
+//                                               @Query("page") String page, @Query("count") String count);
+//
+//    //病友圈请求详情
+//    @GET("user/sickCircle1/findSickCircleInfo")
+//    Observable<Result<ByXiangqingBean>> Byxiangqing(@Header("userId") long id, @Header("sessionId") String sessionId,
+//                                                    @Query("sickCircleId") String sickCircleId);
 
 }
